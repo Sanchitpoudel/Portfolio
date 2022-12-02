156715e800fe92ea16a8754241589d58274e63e8
Sanchit poudel <50201318+Sanchitpoudel@users.noreply.github.com>
2022-12-02 12:43:25 +0545

    commit two

---
+++ test.js
@@ -0,0 +1,4 @@
+var text = "Sanchit Poudel";
+var alphabets='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+document.getElementById('length').innerHTML = "The length of text " + "\"" + text + "\"" + " is " + text.length;
+document.getElementById('slice').innerHTML = alphabets.slice(5, 20);
